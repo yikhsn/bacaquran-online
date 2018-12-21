98870a9aee7abb7d130f46732293c935c65ac40d
Yaumil Ikhsan <yaumilikhsan98@gmail.com>
2018-12-21 19:13:07 +0700

    responsive on surat and surat list

--- resources/views/surat/single.blade.php
+++ resources/views/surat/single.blade.php
@@ -16,7 +16,7 @@
       <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav">
           
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="" id="suratDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
